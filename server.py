468b3616475c0831c8c7303286e82d4249920fac
radu567 <filipesco@mail.ru>
2016-10-11 17:02:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,44 @@
-# server.py
 import socket
+import threading
 
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class ClientThread(threading.Thread):
 
-host = 'localhost'
+    def __init__(self, ip, port):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print("[+] New thread started for "+ip+":"+str(port))
+
+    def run(self):
+        print("Connection from : "+ip+":"+str(port))
+
+#        clientsock.send("\nWelcome to the server\n\n")
+
+        data = clientsock.recv(1024)
+        while data:  # Atit timp cit sunt date
+            print(data)  # Le afisam
+            data = clientsock.recv(1024)  # Citim urmatoarele 1024 bytes de la client
+        clientsock.close()  # Inchidem conexiunea
+
+        print("Client disconnected...")
+
+host = "0.0.0.0"
 port = 9999
-s.bind((host, port))
 
-s.listen(10)
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+tcpsock.bind((host, port))
+threads = []
+
 
 while True:
-    clientsocket, addr = s.accept()
+    tcpsock.listen(4)
+    print("\nListening for incoming connections...")
+    (clientsock, (ip, port)) = tcpsock.accept()
+    newthread = ClientThread(ip, port)
+    newthread.start()
+    threads.append(newthread)
 
-    print("Got a connection from %s" % str(addr))
-    print('client addr: ', addr)
-
-    data = clientsocket.recv(1024)
-
-    if not data:
-        clientsocket.close()
-        break
-    else:
-        print(data)
-    continue
+for t in threads:
+    t.join()
