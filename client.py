f5d0bbf65fb553be3304bef82bb7df4f980575ec
radu567 <filipesco@mail.ru>
2016-10-18 15:32:10 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 # client.py
 import socket
+import json
 
 # create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = 'localhost'
+host = '127.0.0.1'
 port = 9999
 s.connect((host, port))
 
@@ -13,8 +14,11 @@ while True:
     data = input('write to server: ')
     if not data:
         s.close()
-
-    data = str.encode(data)
+    message = {
+        'type': 'send',
+        'message': str(data)
+    }
+    data = json.dumps(message).encode('utf-8')
     s.send(data)
 
 s.close()
